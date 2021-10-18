6773308083e77000b36a88ece4dd288a99629fa7
Ankan Sikdar <ankansikdar@gmail.com>
2021-10-18 21:18:16 +0530

    Added tests for IconButtons

--- test/screens/home_test.dart
+++ test/screens/home_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
@@ -25,6 +27,19 @@ void main() {
           find.byType(ListView), const Offset(0, -200), 3000);
       await widgetTester.pumpAndSettle();
       expect(find.text('Item 0'), findsNothing);
+    });
+
+    testWidgets('Testing IconButtons', (widgetTester) async {
+      await widgetTester.pumpWidget(createHomeScreen());
+      expect(find.byIcon(Icons.favorite), findsNothing);
+      await widgetTester.tap(find.byIcon(Icons.favorite_border).first);
+      await widgetTester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.text('Added to favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsWidgets);
+      await widgetTester.tap(find.byIcon(Icons.favorite).first);
+      await widgetTester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.text('Removed from favorites.'), findsOneWidget);
+      expect(find.byIcon(Icons.favorite), findsNothing);
     });
   });
 }
